49193fbb5a873e4b29d6fbe8b314b906bd52ff79
GarciaMarga <111571160+GarciaMarga@users.noreply.github.com>
2022-09-06 09:26:03 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+ 
+       Student Me = new Student();
+       Me.Surname = "Garcia";
+       Me.firstName = "Ma. Margarita Jana";
+       Me.middleInitial = 'B';
+       Me.dateOfBirth = 01/19/03;
+       Me.studentNumber = 2022100218;
+       Me.emailAddress = "garciamb@students.national-u.edu.ph";
+       Me.iAmAwesome = true;
+       Me.sayMystudentNumber();
+       Me.sayMyEmailAddress();
+       Me.sayIamAwesome();
+    }
+ }
